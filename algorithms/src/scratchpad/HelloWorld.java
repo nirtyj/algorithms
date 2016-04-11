b4965a702d98ef1e7871bc7254c1faa06979b8a6
Nirtyanath Jaganathan <nirtyj@gmail.com>
2016-04-10 18:30:49 -0700

    Initial commit

---
+++ algorithms/src/scratchpad/HelloWorld.java
@@ -0,0 +1,15 @@
+package scratchpad;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		HelloWorld world = new HelloWorld();
+		world.runHelloWorld();
+	}
+
+	public boolean runHelloWorld()
+	{
+		System.out.println("Hello World");
+		return true;
+	}
+}
---
+++ algorithms/tests/scratchpad/HelloWorldTest.java
@@ -0,0 +1,15 @@
+package scratchpad;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+	@Test
+	public void test() {
+		HelloWorld test = new HelloWorld();
+		assertTrue(test.runHelloWorld());
+	}
+
+}
