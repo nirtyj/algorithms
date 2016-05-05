2099c3fda66569ec1de2f7c2469488b01a4a1674
Nirtyanath Jaganathan <nirtyj@gmail.com>
2016-05-04 22:43:50 -0700

    unique paths

---
+++ algorithms/src/leetcode/matrix/UniquePaths.java
@@ -0,0 +1,35 @@
+package leetcode.matrix;
+
+public class UniquePaths {
+
+	public static int uniquePaths(int m, int n) {
+		
+		if(m == 0 || n == 0)
+			return 0;
+		
+		int[][] dp = new int[m][n];
+
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				// check for i-1, j-1 out of matrix range.
+				if((i-1)>=0 && (j-1)>=0)
+				{
+					dp[i][j] = dp[i-1][j] + dp[i][j-1];
+				}
+				else if( (i-1)>=0 && (j -1)<0)
+				{
+					dp[i][j] = dp[i-1][j];
+				}
+				else if( (i-1)<0 && (j -1)>=0)
+				{
+					dp[i][j] =  dp[i][j-1];
+				}
+				else
+				{
+					dp[i][j] = 1;
+				}
+			}
+		}
+		return dp[m - 1][n - 1];
+	}
+}
---
+++ algorithms/tests/leetcode/matrix/UniquePathsTest.java
@@ -0,0 +1,14 @@
+package leetcode.matrix;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UniquePathsTest {
+
+	@Test
+	public void test() {
+		assertEquals(10, UniquePaths.uniquePaths(3, 4));
+	}
+
+}
