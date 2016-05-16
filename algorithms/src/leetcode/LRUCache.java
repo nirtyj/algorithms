9cd06cca36ebd1423a8344d330855cf440e564f3
Nirtyanath Jaganathan <nirtyj@gmail.com>
2016-05-15 17:30:37 -0700

    LRU Cache

---
+++ algorithms/src/leetcode/LRUCache.java
@@ -0,0 +1,90 @@
+package leetcode;
+
+import java.util.HashMap;
+
+public class LRUCache {
+
+	class Node {
+		int key;
+		int value;
+		Node pre;
+		Node next;
+
+		public Node(int key, int value) {
+			this.key = key;
+			this.value = value;
+		}
+	}
+
+	private int capacity;
+	private Node head = null;
+	private Node end = null;
+	private HashMap<Integer, Node> map = new HashMap<>();
+
+	public LRUCache(int capacity) {
+		this.capacity = capacity;
+	}
+
+	public void set(int key, int value) {
+
+		if (map.containsKey(key)) {
+			Node node = map.get(key);
+			node.value = value;
+			remove(node);
+			addAtHead(node);
+
+		} else {
+			Node created = new Node(key, value);
+			if (map.size() >= capacity) {
+				map.remove(end.key);
+				remove(end);
+			}
+			addAtHead(created);
+			map.put(key, created);
+		}
+	}
+
+	private void addAtHead(Node node) {
+
+		Node tempHead = head;
+		if (tempHead == null) {
+			head = node;
+		} else {
+			node.next = tempHead;
+			node.pre = null;
+			tempHead.pre = node;
+			head = node;
+		}
+
+		if (end == null)
+			end = head;
+	}
+
+	private void remove(Node node) {
+		Node prev = node.pre;
+		Node next = node.next;
+		if (prev != null) {
+			prev.next = next;
+		} else {
+			head = next;
+		}
+
+		if (next != null) {
+			next.pre = prev;
+		} else {
+			end = prev;
+		}
+	}
+
+	public int get(int key) {
+
+		if (map.containsKey(key)) {
+			Node n = map.get(key);
+			remove(n);
+			addAtHead(n);
+			return n.value;
+		} else {
+			return -1;
+		}
+	}
+}
---
+++ algorithms/tests/leetcode/LRUCacheTest.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LRUCacheTest {
+
+	@Test
+	public void test() {
+		LRUCache cache = new LRUCache(1);
+		cache.set(2,1);
+		assertEquals(1, cache.get(2));
+		cache.set(3,2);
+		assertEquals(-1, cache.get(2));
+		assertEquals(2, cache.get(3));
+	}
+
+}
