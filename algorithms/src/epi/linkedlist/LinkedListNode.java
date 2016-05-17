ecf3ad5c1d78687ad8cc8bef3e9e1485a1adb884
Nirtyanath Jaganathan <nirtyj@gmail.com>
2016-05-16 19:58:23 -0700

    linkedlist node - toString and formatting

--- algorithms/src/epi/linkedlist/LinkedListNode.java
+++ algorithms/src/epi/linkedlist/LinkedListNode.java
@@ -5,42 +5,36 @@ public class LinkedListNode {
 	public String value;
 	public Integer data;
 	public LinkedListNode next;
-	
+	public LinkedListNode prev;
 
-	public LinkedListNode()
-	{
-		
+	public LinkedListNode() {
+
 	}
-	
-	public LinkedListNode(String value)
-	{
+
+	public LinkedListNode(String value) {
 		this.value = value;
 	}
-	
-	public LinkedListNode(Integer data, LinkedListNode next)
-	{
+
+	public LinkedListNode(Integer data, LinkedListNode next) {
 		this.data = data;
 		this.next = next;
 	}
-	
-	public int size()
-	{
+
+	public int size() {
 		int count = 1;
 		LinkedListNode nextNode = next;
-		while(nextNode!=null)
-		{
+		while (nextNode != null) {
 			count++;
 			nextNode = nextNode.next;
 		}
 		return count;
 	}
-	
+
 	@Override
-	public String toString()
-	{
+	public String toString() {
 		String nextVal = "null";
-		if(next != null)
+		if (next != null)
 			nextVal = next.toString();
-		return "\""+ (this.value == null? this.data : this.value) +"\""+ "->" + nextVal;		
+		return "\"" + (this.value == null ? this.data : this.value) + "\"" + "->" + nextVal;
 	}
 }
