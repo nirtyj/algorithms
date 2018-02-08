f9d18f9b2a78ebd115ec5f16c03d73a57cb7543c
nirtyj <nirtyj@gmail.com>
2018-02-08 09:22:27 -0800

    Simplify Path and Palidrome linked list

--- algorithms/src/epi/linkedlist/PalindromeLinkedList.java
+++ algorithms/src/epi/linkedlist/PalindromeLinkedList.java
@@ -1,5 +1,7 @@
 package epi.linkedlist;
 
+import epi.linkedlist.node.ListNode;
+
 public class PalindromeLinkedList {
 
 	public static boolean isLinkedListAPalindrome(LinkedListNode L) {
@@ -32,5 +34,38 @@ public class PalindromeLinkedList {
 		endofHalf.next = reversedAgainHead;
 		return isPalindrome;
 
+	}
+	
+	public boolean isPalindrome(ListNode head) {
+	    ListNode fast = head, slow = head;
+	    while (fast != null && fast.next != null) {
+	        fast = fast.next.next;
+	        slow = slow.next;
+	    }
+	    if (fast != null) { // odd nodes: let right half smaller
+	        slow = slow.next;
+	    }
+	    slow = reverse(slow);
+	    fast = head;
+	    
+	    while (slow != null) {
+	        if (fast.val != slow.val) {
+	            return false;
+	        }
+	        fast = fast.next;
+	        slow = slow.next;
+	    }
+	    return true;
+	}
+
+	public ListNode reverse(ListNode head) {
+	    ListNode prev = null;
+	    while (head != null) {
+	        ListNode next = head.next;
+	        head.next = prev;
+	        prev = head;
+	        head = next;
+	    }
+	    return prev;
 	}
 }
