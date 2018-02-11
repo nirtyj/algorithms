6e6afa36e831f4d44dbd47ad919785e62c2a6803
nirtyj <nirtyj@gmail.com>
2018-02-10 21:12:02 -0800

    Excep Title Number, Facotrial Trailing Zeros

--- algorithms/src/leetcode/easy/ExcelTitle.java
+++ algorithms/src/leetcode/easy/ExcelTitle.java
@@ -1,9 +1,20 @@
-package leetcode;
+package leetcode.easy;
 
+/**
+Given a positive integer, return its corresponding column title as appear in an Excel sheet.
+For example:
+    1 -> A
+    2 -> B
+    3 -> C
+    ...
+    26 -> Z
+    27 -> AA
+    28 -> AB 
+*/
 public class ExcelTitle {
+
 	/**
 	 * Leetcode verified
-	 * 
 	 * @param n
 	 * @return
 	 */
--- algorithms/tests/leetcode/ExcelTitleTest.java
+++ algorithms/tests/leetcode/ExcelTitleTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import leetcode.easy.ExcelTitle;
+
 public class ExcelTitleTest {
 
 	@Test
