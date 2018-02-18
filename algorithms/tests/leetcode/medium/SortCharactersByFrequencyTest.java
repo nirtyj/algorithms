8687169c121f386f2a4479f0fa4505ae45b373c8
nirtyj <nirtyj@gmail.com>
2018-02-17 16:14:00 -0800

    Numbers disappeared, Power of Two, SortCharactersByFrequency, Find peak Element, Guess HigherOrLower, TopKFrequentElements

---
+++ algorithms/tests/leetcode/medium/SortCharactersByFrequencyTest.java
@@ -0,0 +1,14 @@
+package leetcode.medium;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SortCharactersByFrequencyTest {
+
+	@Test
+	public void test() {
+		assertEquals("eeeeaaarrd",SortCharactersByFrequency.frequencySort("raaeaedere"));
+	}
+
+}
