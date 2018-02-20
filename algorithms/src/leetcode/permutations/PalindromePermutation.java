758b286eac680ba87a5f870018e49b10b2dc5f48
nirtyj <nirtyj@gmail.com>
2018-02-19 16:19:57 -0800

    Next Perumutation, Palindrome Permutation, II, Permutation Sequence, Palindromic Permutation, II

---
+++ algorithms/src/leetcode/permutations/PalindromePermutation.java
@@ -0,0 +1,28 @@
+package leetcode.permutations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+Given a string, determine if a permutation of the string could form a palindrome.
+
+For example,
+"code" -> False, "aab" -> True, "carerac" -> True.
+*/
+public class PalindromePermutation {
+	/**
+	 * Leetcode verified
+	 * @param s
+	 * @return
+	 */
+	public boolean canPermutePalindrome(String s) {
+		Set<Character> set = new HashSet<>();
+		for (Character c : s.toCharArray()) {
+			if (set.contains(c))
+				set.remove(c);
+			else
+				set.add(c);
+		}
+		return set.size() < 2;
+	}
+}
