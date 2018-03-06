c82f471432dc690d289b645e899753cf4678eff9
nirtyj <nirtyj@gmail.com>
2018-03-05 19:42:47 -0800

    remove duplicates sorted array

--- algorithms/src/leetcode/medium/RemoveDuplicatesFromSortedArrayII.java
+++ algorithms/src/leetcode/medium/RemoveDuplicatesFromSortedArrayII.java
@@ -11,12 +11,17 @@ Your function should return length = 5, with the first five elements of nums bei
 */
 public class RemoveDuplicatesFromSortedArrayII {
 
+	/**
+	 * Leetcode verified
+	 * @param nums
+	 * @return
+	 */
 	public static int removeDuplicates(int[] nums) {
 		int i = 0;
 		for (int n : nums)
 		{
-			// for starting 0, 1 - let it pass through
-			// if current number is greater than previous, copy
+			// for starting 0, 1 - let it pass through - doesnt matter as the max is going to be 2 anways.
+			// if current number is greater than previous number. i.e i-2, copy - for rest of the duplicates, it ignores copying
 			if (i < 2 || n > nums[i - 2])
 			{
 				nums[i] = n;
