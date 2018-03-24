5f4613c303f5c03d1d7b2afac246145acfa243c6
nirtyj <nirtyj@gmail.com>
2018-03-24 12:30:39 -0700

    coin change, coin change II

--- interviewchallenges/facebook/careercup/facebook/CoinChange.java
+++ interviewchallenges/facebook/careercup/facebook/CoinChange.java
@@ -1,9 +1,27 @@
 package careercup.facebook;
 
 /**
-Find out how many ways you can make change of N cents given coins of values [a1, a2, a3...], each type of coin got infinite supplies. 
-Arrangement of coins don't matter. 
-*/
+ * Find out how many ways you can make change of N cents given coins of values
+ * [a1, a2, a3...], each type of coin got infinite supplies. Arrangement of
+ * coins don't matter.
+ */
 public class CoinChange {
-
+	
+	/**
+	 * 
+	 * @param amount
+	 * @param coins
+	 * @return
+	 */
+	public int change(int amount, int[] coins) {
+		int[] dp = new int[amount + 1];
+		dp[0] = 1;
+		for (int coin : coins) {
+			for (int i = 1; i <= amount; i++) {
+				if (i >= coin)
+					dp[i] += dp[i - coin];
+			}
+		}
+		return dp[amount];
+	}
 }
