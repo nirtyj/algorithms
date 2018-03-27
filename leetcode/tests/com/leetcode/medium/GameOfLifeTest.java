51d2c3d4da4ce8659b3071fb8099997e121ba4d8
nirtyj <nirtyj@gmail.com>
2018-03-27 06:47:10 -0700

    Encode and decode strings, game of life

---
+++ leetcode/tests/com/leetcode/medium/GameOfLifeTest.java
@@ -0,0 +1,17 @@
+package com.leetcode.medium;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GameOfLifeTest {
+
+	@Test
+	public void test() {
+		int[][] board = {{1,1},{1,0}};
+		GameOfLife life = new GameOfLife();
+		life.gameOfLife(board);
+		System.out.println();
+	}
+
+}
