d20913d27698d5ec708b28681194897a3936173b
nirtyj <nirtyj@gmail.com>
2018-03-27 09:21:31 -0700

    Longest Univalue Path, Two SumIII

---
+++ leetcode/src/com/leetcode/easy/LongestUnivaluePath.java
@@ -0,0 +1,63 @@
+package com.leetcode.easy;
+
+import com.leetcode.common.TreeNode;
+
+/**
+Given a binary tree, find the length of the longest path where each node in the path has the same value. This path may or may not pass through the root.
+
+Note: The length of path between two nodes is represented by the number of edges between them.
+
+Example 1:
+
+Input:
+
+              5
+             / \
+            4   5
+           / \   \
+          1   1   5
+Output:
+
+2
+Example 2:
+
+Input:
+
+              1
+             / \
+            4   5
+           / \   \
+          4   4   5
+Output:
+
+2
+Note: The given binary tree has not more than 10000 nodes. The height of the tree is not more than 1000.
+*/
+public class LongestUnivaluePath {
+
+	int len = 0; // global variable
+	/**
+	 * Leetcode verified
+	 * @param root
+	 * @return
+	 */
+	public int longestUnivaluePath(TreeNode root) {
+		len = 0;
+		if (root == null)
+			return len;
+
+		getLen(root, root.val);
+		return len;
+	}
+
+	private int getLen(TreeNode node, int rootVal) {
+		if (node == null)
+			return 0;
+		int left = getLen(node.left, node.val);
+		int right = getLen(node.right, node.val);
+		len = Math.max(len, left + right);
+		if (rootVal == node.val)
+			return Math.max(left, right) + 1;
+		return 0;
+	}
+}
