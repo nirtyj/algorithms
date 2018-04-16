e2ed9f712464f63550abf229c560122e45c48aff
nirtyj <nirtyj@gmail.com>
2018-04-15 18:07:45 -0700

    TaskScheduler and reorganize string

---
+++ leetcode/src/com/leetcode/medium/ReOrganizeString.java
@@ -0,0 +1,63 @@
+package com.leetcode.medium;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+Given a string S, check if the letters can be rearranged so that two characters that are adjacent to each other are not the same.
+
+If possible, output any possible result.  If not possible, return the empty string.
+
+Example 1:
+
+Input: S = "aab"
+Output: "aba"
+Example 2:
+
+Input: S = "aaab"
+Output: ""
+Note:
+
+S will consist of lowercase letters and have length in range [1, 500].
+*/
+public class ReOrganizeString {
+
+	public String reorganizeString(String s) {
+		HashMap<Character, Integer> frequency = new HashMap<>();
+		for (int i = 0; i < s.length(); i++) {
+			int val = frequency.getOrDefault(s.charAt(i), 0) + 1;
+			frequency.put(s.charAt(i), val);
+		}
+
+		PriorityQueue<Map.Entry<Character, Integer>> maxHeap = new PriorityQueue<>(
+				(a, b) -> a.getValue() != b.getValue() ? b.getValue() - a.getValue() : a.getKey() - b.getKey());
+		maxHeap.addAll(frequency.entrySet());
+
+		Queue<Map.Entry<Character, Integer>> waitQueue = new LinkedList<>();
+		StringBuilder rearranged = new StringBuilder();
+		int k = 2; // it has to be 2 spaces apart
+		while (!maxHeap.isEmpty()) {
+
+			Map.Entry<Character, Integer> current = maxHeap.poll();
+			rearranged.append(current.getKey());
+			current.setValue(current.getValue() - 1);
+			waitQueue.offer(current);
+
+			if (waitQueue.size() < k) { // intial k-1 chars, waitQueue not full yet
+				continue;
+			}
+			// release from waitQueue if char is already k apart
+			Map.Entry<Character, Integer> front = waitQueue.poll();
+			// note that char with 0 count still needs to be placed in waitQueue as a place
+			// holder
+			if (front.getValue() > 0) {
+				maxHeap.offer(front);
+			}
+		}
+
+		return rearranged.length() == s.length() ? rearranged.toString() : "";
+	}
+}
--- leetcode/src/com/leetcode/medium/TaskScheduler.java
+++ leetcode/src/com/leetcode/medium/TaskScheduler.java
@@ -1,9 +1,14 @@
 package com.leetcode.medium;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
 
 /**
-Given a char array representing tasks CPU need to do. It contains capital letters A to Z where different letters represent different tasks.Tasks could be done without original order. Each task could be done in one interval. For each interval, CPU could finish one task or just be idle.
+Given a char array representing tasks CPU need to do. It contains capital letters A to Z where different letters represent different tasks.Tasks could be done without original order. 
+Each task could be done in one interval. For each interval, CPU could finish one task or just be idle.
 
 However, there is a non-negative cooling interval n that means between two same tasks, there must be at least n intervals that CPU are doing different tasks or just be idle.
 
@@ -23,25 +28,38 @@ public class TaskScheduler {
 	 * @return
 	 */
 	public int leastInterval(char[] tasks, int n) {
-        int[] map = new int[26];
-        for (char c: tasks)
-            map[c - 'A']++;
-        // the task with biggest number of task goes to the right
-        Arrays.sort(map);
-        int time = 0;
-        while (map[25] > 0) {
-            int i = 0; // cooling time counter
-            while (i <= n) { // until you get heated, pick up from the right to left.
-                if (map[25] == 0)
-                    break;
-                if (i < 26 && map[25 - i] > 0)
-                    map[25 - i]--;
-                time++;
-                i++;
-            }
-            // everytime you breakout for a new cooldown, re-order the array
-            Arrays.sort(map);
-        }
-        return time;
-    }
+		HashMap<Character, Integer> frequency = new HashMap<>();
+		for (Character c : tasks) {
+			int val = frequency.getOrDefault(c, 0) + 1;
+			frequency.put(c, val);
+		}
+
+		PriorityQueue<Map.Entry<Character, Integer>> queue = new PriorityQueue<>(
+				(a, b) -> a.getValue() != b.getValue() ? b.getValue() - a.getValue() : a.getKey() - b.getKey());
+		queue.addAll(frequency.entrySet());
+
+		int result = 0;
+		while (!queue.isEmpty()) {
+			int cooldown = n + 1; // increase 1 because you do not cool down when you remove the first one.
+			List<Map.Entry<Character,Integer>> tempList = new ArrayList<>();
+			while (cooldown > 0 && !queue.isEmpty()) {
+				Map.Entry<Character, Integer> top = queue.poll();
+				top.setValue(top.getValue() - 1);// decrease frequency, meaning it got executed
+				tempList.add(top);// collect task to add back to queue
+				result++;
+				cooldown--;
+			}
+
+			for (Map.Entry<Character, Integer> entry : tempList) {
+				if (entry.getValue() > 0) // add only if there are more than 0 tasks
+					queue.offer(entry);
+			}
+			if (queue.isEmpty()) // all tasks are executed
+				break;
+
+			result = result + cooldown; // need to add cool down because, all tasks have been executed but cooldown is higher
+
+		}
+		return result;
+	}
 }
