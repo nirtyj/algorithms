0023fcf0660c5e1e8b9c63d9279d766d5488dfa0
nirtyj <nirtyj@gmail.com>
2018-05-01 21:30:41 -0700

    Jewel & stones, LFU cache, Merge trees

---
+++ leetcode/src/com/leetcode/easy/JewelsAndStones.java
@@ -0,0 +1,46 @@
+package com.leetcode.easy;
+
+import java.util.HashMap;
+/**
+You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+
+The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+Example 1:
+
+Input: J = "aA", S = "aAAbbbb"
+Output: 3
+Example 2:
+
+Input: J = "z", S = "ZZ"
+Output: 0
+Note:
+
+S and J will consist of letters and have length at most 50.
+The characters in J are distinct.
+*/
+public class JewelsAndStones {
+
+	/**
+	 * Leetcode verified
+	 * @param J
+	 * @param S
+	 * @return
+	 */
+	public int numJewelsInStones(String J, String S) {
+		if (J == null || J.length() == 0 || S == null || S.length() == 0)
+			return 0;
+		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+		for (int i = 0; i < J.length(); i++)
+			map.put(J.charAt(i), 0);
+		for (int i = 0; i < S.length(); i++) {
+			if (map.containsKey(S.charAt(i))) {
+				map.put(S.charAt(i), map.get(S.charAt(i)) + 1);
+			}
+		}
+		int sum = 0;
+		for (Integer val : map.values())
+			sum = sum + val;
+		return sum;
+	}
+}
---
+++ leetcode/src/com/leetcode/hard/LFUCache.java
@@ -0,0 +1,76 @@
+package com.leetcode.hard;
+
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+
+/**
+Design and implement a data structure for Least Frequently Used (LFU) cache. It should support the following operations: get and put.
+
+get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
+put(key, value) - Set or insert the value if the key is not already present. When the cache reaches its capacity, it should invalidate the least frequently used item before inserting a new item. For the purpose of this problem, when there is a tie (i.e., two or more keys that have the same frequency), the least recently used key would be evicted.
+
+Follow up:
+Could you do both operations in O(1) time complexity?
+*/
+public class LFUCache {
+
+	HashMap<Integer, Integer> vals; // holds actual value
+	HashMap<Integer, Integer> counts; // for frequency
+	HashMap<Integer, LinkedHashSet<Integer>> lists; // frequency -> key1, key2..
+	int cap;
+	int min = 0;
+
+	public LFUCache(int capacity) {
+		cap = capacity;
+		vals = new HashMap<>();
+		counts = new HashMap<>();
+		lists = new HashMap<>();
+		lists.put(1, new LinkedHashSet<>());
+	}
+
+	public int get(int key) {
+		if (!vals.containsKey(key))
+			return -1;
+		// increase the counter,
+		int count = counts.get(key);
+		counts.put(key, count + 1);
+
+		// remove the key from previous lists
+		lists.get(count).remove(key);
+		// update the new key to lists
+		if (!lists.containsKey(count + 1))
+			lists.put(count + 1, new LinkedHashSet<>());
+		lists.get(count + 1).add(key);
+
+		// update the minimum
+		if (count == min && lists.get(count).size() == 0)
+			min++;
+
+		return vals.get(key);
+	}
+
+	public void put(int key, int value) {
+		if (cap <= 0)
+			return;
+		// if it contains, just increase the access count. after updating the value
+		if (vals.containsKey(key)) {
+			vals.put(key, value);
+			get(key);
+			return;
+		}
+		// if its a new value, check if it exceeds the capacity. if yes, remove the
+		// mins...
+		if (vals.size() >= cap) {
+			int evit = lists.get(min).iterator().next();
+			lists.get(min).remove(evit);
+			vals.remove(evit);
+			counts.remove(evit);
+		}
+
+		// now, add the new value, update min = 1
+		vals.put(key, value);
+		counts.put(key, 1);
+		min = 1;
+		lists.get(1).add(key);
+	}
+}
