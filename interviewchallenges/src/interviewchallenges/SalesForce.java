39ca57cfd58897a5105a9a9201dc5a7c43f26e08
nirtyj <nirtyj@gmail.com>
2018-05-11 09:54:18 -0700

    adding sales force questions

---
+++ interviewchallenges/src/interviewchallenges/SalesForce.java
@@ -0,0 +1,82 @@
+package interviewchallenges;
+
+public class SalesForce {
+
+	/**
+	 * Pure Javascript, CSS, QA, BFS, DFS, Sorting, Map-Reduce, Database, System Design, Tree problems
+	 * 
+	 * here were 3 technical rounds 
+	 * and a lunch interview with the team's manage
+	 * 
+	 * Describe how to architect a web server architecture and list out various control points and security implications
+	 * OOPs on a Uber like application
+	 * 
+	 * Programming questions including implementing rate limiting for a web server and return coin denominations question.
+	 * 
+	 * Variant of max sum in a binary tree (not a BST).
+	 * 
+	 * A design question related to scalability
+	 * An algorithm question involving binary search tree.
+	 * 
+	 * QA testing question
+	 * LRU cache, merge ranges,
+	 * Find the first unique character in a string
+	 * Balancing curly brackets, parens, and square brackets 
+	 * String manipulation, probability, SQL, sorting, systems design, class implementation, test cases, complexity
+	 * 
+	 * Front-end, such as html, css and js, to Back-end, such as oo design, SQL, Java language.
+	 *  Also, Salesforce also wants developers have a mindset of testing. Therefore, keep some basic testing knowledge as well.
+	 *  
+	 *  Code high level class hierarchy to implement a Library system that includes Library book checkouts, returns, 
+	 *  search based on title, partial title search, author, ISBN and other meta data.
+	 *  library fining program that keeps tracks of the fines of individual items in the library based on customer.
+	 *  operating system internals, file system internals, computer architecture, distributed systems, coding, data structures, 
+	 *  complexity analysis, design choices, past projects and challenges encountered, 
+	 *  producer-consumer variations, multi-threaded programming related and such
+	 *  
+	 *  Interview 1 - Medium level question followed by an extension.
+		Lunch - More information about the team and role
+		Interview 2 - One easy Tree question and one medium DP question. Nothing unexepected
+		Interview 3 - One medium question followed by a lengthy discussion on one of the sorting algorithms. Didn do to well in this interview
+		Interview 4 - A basic design question, some design pattern discussion and REST discussion 
+		
+		understand my interest, why salesforce etc.
+		
+		Design Dropbox. Design a distrubuted NoSQL. Coding in circular buffer and readN and a bunch of others
+		
+		Write an efficient function that checks whether any permutation of an input string is a palindrome
+		Find duplicate files.
+		counts in word cloud data 
+		
+		sorting an array
+		
+		data size, traffic, API, request flow, load balancers, security, session replication, fault tolerance, sharding
+		
+		Resume, Reverse string (collections & 2 pointers), garbage collector, 
+		how hashmap works in java, Find connecting points of 2 linked list, find max path sum for a tree. LeetCode - Medium & easy
+		
+		sort a list of numbers by the frequency of each number in the list, then optimize it
+		
+		
+		1) programming fundamentals (OOP vs FP, some data structure basics, Java basics etc), 
+		2) coding challenge questions + behavioral questions, 
+		3) design interview,
+		4) distributed systems interview
+		
+		 on-site interview, each interview consisted of two engineers (one QA and one Dev). 
+		 The first 30-40 minutes of each interview consisted of design/algorithm questions and the last 10-15 minutes were focused on QA questions 
+		 regarding the designs that I've came up with. 
+		 Salesforce has recently combined the QA and Dev roles into one, calling it the 'hybrid' system. 
+		 Thus, each interview consisted an engineer from QA and Dev
+		 
+		 Can you write an algorithm that computes the nth Fibonacci number in linear time or faster?	
+		 
+		 Creating, handling, and preventing deadlocks and livelocks
+		 What's the difference between an abstract class and an interface?
+		 How would you implement a Stack if you're given access to a Queue API and only the Queue API? 
+		 Use two stacks to implement a queue.
+		 Find the median in an unsorted linked list
+		 Datacenter/SQL question about joins and ordering  
+		 Print out the boundary nodes of a binary tree 
+	 */
+}
