ede34446f2a3b85c27e3fa39198e2e1e38648c6f
nirtyj <nirtyj@gmail.com>
2018-06-06 00:27:17 -0700

    largestbstsubtree

---
+++ leetcode/tests/com/leetcode/trees/LargestBSTSubTreeTest.java
@@ -0,0 +1,72 @@
+package com.leetcode.trees;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.leetcode.common.TreeNode;
+
+public class LargestBSTSubTreeTest {
+
+	@Test
+	public void test() {
+		LargestBSTSubTree t = new LargestBSTSubTree();
+		TreeNode root = new TreeNode(10);
+		
+		TreeNode n5 = new TreeNode(5);
+		
+		TreeNode n1 = new TreeNode(1);
+		TreeNode n8 = new TreeNode(8);
+		
+		TreeNode n15 = new TreeNode(15);
+		
+		TreeNode n7 = new TreeNode(7);
+		
+		root.left = n5;
+		root.right = n15;
+		n5.left = n1;
+		n5.right = n8;
+		n15.right = n7;
+		assertEquals(3, t.largestBSTSubtree(root));
+	}
+	
+	@Test
+	public void test2() {
+		LargestBSTSubTree t = new LargestBSTSubTree();
+		TreeNode root = new TreeNode(1);
+		
+		TreeNode n3 = new TreeNode(3);
+	
+		TreeNode n4 = new TreeNode(4);
+		TreeNode n2 = new TreeNode(2);
+		
+		TreeNode n5 = new TreeNode(5);
+		
+		root.left = n3;
+		root.right = n2;
+		n3.left = n4;
+		n2.right = n5;
+		assertEquals(2, t.largestBSTSubtree(root));
+	}
+	
+	@Test
+	public void test3() {
+		LargestBSTSubTree t = new LargestBSTSubTree();
+		TreeNode root = new TreeNode(4);
+		
+		TreeNode n1 = new TreeNode(1);
+	
+		TreeNode n6 = new TreeNode(6);
+		
+		TreeNode n5 = new TreeNode(5);
+		
+		TreeNode n2 = new TreeNode(2);
+		
+		root.left = n1;
+		root.right = n6;
+		n6.left = n5;
+		n5.left = n2;
+		assertEquals(3, t.largestBSTSubtree(root));
+	}
+
+}
