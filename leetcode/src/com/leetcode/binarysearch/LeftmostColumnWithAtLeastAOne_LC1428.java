741458629f96442761a28b0871def492b5678ac6
nirtyj <nirtyj@gmail.com>
2020-12-29 08:15:02 -0800

    Binary Search within Matrix added best solution - LC1428

--- leetcode/src/com/leetcode/binarysearch/LeftmostColumnWithAtLeastAOne_LC1428.java
+++ leetcode/src/com/leetcode/binarysearch/LeftmostColumnWithAtLeastAOne_LC1428.java
@@ -25,6 +25,38 @@ public class LeftmostColumnWithAtLeastAOne_LC1428 {
       public List<Integer> dimensions(){return new ArrayList<>();};
     };
 
+    /**
+     * O (n + m)
+     * @param binaryMatrix
+     * @return
+     */
+    public int leftMostColumnWithOneBest(BinaryMatrix binaryMatrix) {
+
+        int rows = binaryMatrix.dimensions().get(0);
+        int cols = binaryMatrix.dimensions().get(1);
+
+        // Set pointers to the top-right corner.
+        int currentRow = 0;
+        int currentCol = cols - 1;
+
+        // Repeat the search until it goes off the grid.
+        while (currentRow < rows && currentCol >= 0) {
+            if (binaryMatrix.get(currentRow, currentCol) == 0) {
+                currentRow++;
+            } else {
+                currentCol--;
+            }
+        }
+
+        // If we never left the last column, this is because it was all 0's.
+        return (currentCol == cols - 1) ? -1 : currentCol + 1;
+    }
+
+    /**
+     * O (n * log(m))
+     * @param binaryMatrix
+     * @return
+     */
     public int leftMostColumnWithOne(BinaryMatrix binaryMatrix) {
         List<Integer> dimensions = binaryMatrix.dimensions();
         int rows = dimensions.get(0);
