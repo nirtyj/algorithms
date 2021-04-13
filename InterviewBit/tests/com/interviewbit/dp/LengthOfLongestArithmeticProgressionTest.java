c0adef6c2e0322f358760b03ba541a70ba118ca7
nirtyj <nirtyj@gmail.com>
2021-04-13 07:12:14 -0700

    updating maven with junit dependencies

---
+++ InterviewBit/tests/com/interviewbit/dp/LengthOfLongestArithmeticProgressionTest.java
@@ -0,0 +1,16 @@
+package com.interviewbit.dp;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class LengthOfLongestArithmeticProgressionTest {
+
+    @Test
+    public void solve() {
+        LengthOfLongestArithmeticProgression p = new LengthOfLongestArithmeticProgression();
+        p.solve(new ArrayList<>());
+    }
+}
