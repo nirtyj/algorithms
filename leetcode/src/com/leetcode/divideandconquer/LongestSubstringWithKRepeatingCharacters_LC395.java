c2b4963033d8b7282a1a4a81e5638cdb65593bd3
nirtyj <nirtyj@gmail.com>
2021-04-21 17:53:01 -0700

    LC395

---
+++ leetcode/src/com/leetcode/divideandconquer/LongestSubstringWithKRepeatingCharacters_LC395.java
@@ -0,0 +1,38 @@
+package com.leetcode.divideandconquer;
+
+public class LongestSubstringWithKRepeatingCharacters_LC395 {
+
+    /**
+     * Divide and conquer - O(n*2)
+     * @param s
+     * @param k
+     * @return
+     */
+    public int longestSubstring(String s, int k) {
+        if (s == null || s.length() == 0) return 0;
+        char[] chars = new char[26];
+        // record the frequency of each character
+        for (int i = 0; i < s.length(); i += 1) chars[s.charAt(i) - 'a'] += 1;
+        boolean flag = true;
+        // check for the whole string return the length of string if this string is a valid string
+        for (int i = 0; i < chars.length; i += 1) {
+            if (chars[i] < k && chars[i] > 0) flag = false;
+        }
+        if (flag == true)
+            return s.length();
+
+        // divide and conquer
+        int result = 0;
+        int start = 0, cur = 0;
+        // otherwise we use all the infrequent elements as splits
+        while (cur < s.length()) {
+            if (chars[s.charAt(cur) - 'a'] < k) {
+                result = Math.max(result, longestSubstring(s.substring(start, cur), k));
+                start = cur + 1;
+            }
+            cur++;
+        }
+        result = Math.max(result, longestSubstring(s.substring(start), k)); // do it for the last part.
+        return result;
+    }
+}
